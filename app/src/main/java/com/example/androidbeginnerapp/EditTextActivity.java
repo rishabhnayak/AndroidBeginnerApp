30e209d3773e8c7285bb1d5cf6a90f93609a4335
Rishabh Nayak <rishabhnayak76@gmail.com>
2020-11-28 15:02:15 +0530

    Button Drawable,New Java Activity,manifest file

---
+++ app/src/main/java/com/example/androidbeginnerapp/EditTextActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidbeginnerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout._8_edittext);
+    }
+}
--- app/src/main/java/com/example/androidbeginnerapp/MainActivity.java
+++ app/src/main/java/com/example/androidbeginnerapp/MainActivity.java
@@ -3,13 +3,46 @@ package com.example.androidbeginnerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    //1. Initialize object
+    Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout._7_login);
-        getActionBar().hide();
+        //Hide Action Bar
+        getSupportActionBar().hide();
+
+        // find view
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //3. make your on method
+                Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button 2 Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void button1(View view) {
+        Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void button2(View view) {
+        Toast.makeText(MainActivity.this, "Button 2 Clicked", Toast.LENGTH_SHORT).show();
     }
 }
