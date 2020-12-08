afe4b5fa972aa75dfc6d46ea3a7292a9f8fce043
Rishabh Nayak <rishabhnayak76@gmail.com>
2020-12-08 17:38:29 +0530

    Easyfont,DatePicker

---
+++ app/src/main/java/com/example/androidbeginnerapp/FontActivity.java
@@ -0,0 +1,39 @@
+package com.example.androidbeginnerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.koonat.easyfont.TextView;
+
+import java.util.Calendar;
+
+public class FontActivity extends AppCompatActivity {
+    DatePickerDialog datepicker;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout._10_activity_font);
+       textView = findViewById(R.id.text);
+    }
+
+    public void datePicker(View view) {
+        final Calendar cldr = Calendar.getInstance();
+        int day = cldr.get(Calendar.DAY_OF_MONTH);
+        int month = cldr.get(Calendar.MONTH);
+        int year = cldr.get(Calendar.YEAR);
+        // date picker dialog
+        datepicker = new DatePickerDialog(FontActivity.this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        textView.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                }, year, month, day);
+        datepicker.show();
+    }
+}
